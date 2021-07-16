b3be1b471b013f12d4585571f059b207fd82a803
Andy Wei <andy.wei@techmore.com.tw>
2021-07-16 15:21:24 +0800

    no message

--- redirect_uri.js
+++ redirect_uri.js
@@ -1,6 +1,9 @@
+// LINE Notify Redirect_Uri：https://script.google.com/macros/s/AKfycbzdYblEGSFew9yw7w_vKOnzWuMwucDcePF45VhMOpDKJhMrOApW8M-xRh7FU4Ivclf0/exec
 function doGet(e) {
+  //這裡的都是不一定要用Get的功能，因為用Google AppScript無法準備多支介接只好出此下策，正常應該分成好幾支API處理
   var type = e.parameter.type
   if (type == "state") {
+    //隨機產生一組state字串並與傳入的cellphone綁定寫到State資料表
     var cellphone = e.parameter.cellphone
     var state = getRandomString()
     var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("State")
@@ -15,6 +18,7 @@ function doGet(e) {
     sheet.getRange(lastRow + 1, 2).setValue(state)
     return ContentService.createTextOutput(state)
   }else if (type == "send") {
+    //根據傳入的cellphone找出AccessToken並發送訊息
     var msg = e.parameter.msg
     var cellphone = e.parameter.cellphone
     var token = ""
@@ -58,27 +62,32 @@ function doPost(e) {
   var code = e.parameter.code
   var state = e.parameter.state
   var cellphone = ""
+  //↓↓利用state取出存在State表中的cellphone↓↓
   var stateSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("State")
   var lastRow = stateSheet.getLastRow()
   for (var i = 1; i<=lastRow; i++) {
     if (stateSheet.getRange(i, 2).getDisplayValue() == state) {
+      //↓↓透過state找出對應的cellphone
       cellphone = stateSheet.getRange(i, 1).getDisplayValue()
+      //↓↓刪掉該行state的資料↓↓
       stateSheet.deleteRow(i)
     }
   }
   if (cellphone == "") {
+    //找不到cellphone
     return ContentService.createTextOutput("Can't find cellphone")
   }
+  //↓↓取出redirect_uri，因為無法直接寫在程式中，只好另外讀取↓↓
   var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Callback")
   var url = sheet.getRange("A1").getDisplayValue()
+  //↓↓呼叫取得AccessToken的LINE Notify API
   var api = "https://notify-bot.line.me/oauth/token"
-  
   var formData = {
     'grant_type': 'authorization_code',
-    'code': code,
-    'redirect_uri': url,
-    'client_id': 'nQTAPEeeT6MPcZA15ghfV3',
-    'client_secret': 'UFOITvEiSNc9bDPCxh8yDVz36HPalDxxM8F99EtiVTO'
+    'code': code, //LINE API回傳的code，用來取得AccessToken，無法論成與否都只能使用一次
+    'redirect_uri': url, //如果這邊帶的redirect_uri與後台設定的不一致會失敗
+    'client_id': 'nQTAPEeeT6MPcZA15ghfV3', //註冊服務時會取得
+    'client_secret': 'UFOITvEiSNc9bDPCxh8yDVz36HPalDxxM8F99EtiVTO' //註冊服務時取得
   }
   var response = UrlFetchApp.fetch(api, {
     "headers": {
@@ -89,7 +98,7 @@ function doPost(e) {
   })
   var json = JSON.parse(response.getContentText())
   var token = json.access_token
-  
+  //↓↓將AccessToken與手機寫回資料表，以便透過手機號碼取得AccessToken↓↓
   var tokenSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("AccessToken")
   var lastRow = tokenSheet.getLastRow()
   for (var i = 1; i<=lastRow; i++) {
@@ -100,6 +109,6 @@ function doPost(e) {
   }
   tokenSheet.getRange(lastRow + 1, 1).setValue("'"+cellphone)
   tokenSheet.getRange(lastRow + 1, 2).setValue(state)
-  
+  //回傳response
   return ContentService.createTextOutput("綁定完成")
 }
